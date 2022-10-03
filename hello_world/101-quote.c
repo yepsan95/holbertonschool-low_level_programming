740c5a117063decddb8ccd1389f6e2fc4bfe2abc
Bryan Yep <yepsan95@gmail.com>
2022-10-03 16:54:52 -0700

    Prints without printf() or puts()

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
  * main - C primary function
@@ -6,6 +8,8 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	char text1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, text1, strlen(text1)); 
 	return (1);
+
 }
