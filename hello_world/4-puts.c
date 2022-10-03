32174138bd71b3e56fbb3e3261c6f26e3abaeec2
Bryan Yep <yepsan95@gmail.com>
2022-10-03 12:33:09 -0700

    C program that prints a specific string

---
+++ hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/* This script prints a specific string with the puts() command */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
