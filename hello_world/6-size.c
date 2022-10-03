caf9d2ff82ef95efb09076bd46b8f0136d3985c6
Bryan Yep <yepsan95@gmail.com>
2022-10-03 13:23:58 -0700

    Size of data types

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - C primary function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n
+		Size of an int: %zu byte(s)\n
+		Size of a long int : %zu byte(s)\n
+		Size of a long long int: %zu byte(s)\n
+		Size of a float: %zu byte(s)\n",
+		sizeof(char),
+		sizeof(int),
+		sizeof(long int),
+		sizeof(long long int),
+		sizeof(float))
+	return (0)
+}
