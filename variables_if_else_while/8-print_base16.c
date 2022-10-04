caf1162037e3031d5f5c768458b5ee581edc9fd5
Bryan Yep <yepsan95@gmail.com>
2022-10-04 14:19:42 -0700

    Prints all single digits from base 16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (ch1 = '0'; ch1 <= '9'; ch1++)
 		putchar(ch1);
-	for (ch2 = 'A'; ch2 <= 'F'; ch2++)
+	for (ch2 = 'a'; ch2 <= 'f'; ch2++)
 		putchar(ch2);
 	putchar(newline);
 	return (0);
