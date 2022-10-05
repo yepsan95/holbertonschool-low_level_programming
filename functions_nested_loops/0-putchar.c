55ea3c3695a212b58b370a72a5eb251ae84d50e1
Bryan Yep <yepsan95@gmail.com>
2022-10-05 09:17:22 -0700

    Prints '_putchar'

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - C primary functions
+ * Return: 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar(10);
+	return (0);
+}
