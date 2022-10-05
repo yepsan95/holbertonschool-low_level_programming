9f722c76d3cc48f628c42d97a6202fe5c4998e33
Bryan Yep <yepsan95@gmail.com>
2022-10-04 17:17:59 -0700

    Prints all two digit combinations without repeating

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ *  main - C primary function
+ *  Return: 0
+ */
+int main(void)
+{
+	int ch1;
+	int ch2;
+	int newline = 10;
+
+	ch1 = 48;
+	while (ch1 <= 57)
+	{
+		ch2 = 48;
+		while (ch2 <= 57)
+		{
+			if (ch1 < ch2)
+			{	putchar(ch1);
+				putchar(ch2);
+				if (!(ch1 == 56 && ch2 ==57))
+				{
+					putchar(',');
+					putchar(32);
+					ch2++;
+				}
+				else
+					ch2++;
+			}
+			else
+			{
+				ch2++;
+				continue;
+			}
+		}
+		ch1++;
+	}
+	putchar(newline);
+	return (0);
+}
