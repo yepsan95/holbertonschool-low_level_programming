fb68fd7602b5200dadbb9e7eacb2f726c5ff60e4
Bryan Yep <yepsan95@gmail.com>
2022-10-05 14:44:59 -0700

    Prints alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - C primary function
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	
+	for (n = 97; n <= 122; n++)
+		_putchar(n);
+	return (0);
+}
