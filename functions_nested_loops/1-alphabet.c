7d916e14701eaf5d13fbfbce27c8fde4876df32f
Bryan Yep <yepsan95@gmail.com>
2022-10-05 15:06:48 -0700

    Function that prints the alphabet in lowe case

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - C primary function
  * Return: 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int n;
 
 	for (n = 97; n <= 122; n++)
 		_putchar(n);
 	_putchar(10);
-	return (0);
 }
