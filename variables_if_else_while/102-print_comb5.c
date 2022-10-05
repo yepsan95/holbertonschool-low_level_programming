a3938b37c826f0b970b4627727ec2fad09532b2e
Bryan Yep <yepsan95@gmail.com>
2022-10-04 23:45:09 -0700

    Prints all combinations of 2 digit numbers

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/**
+ *  main - C primary function
+ *  Return: 0
+ */
+int main(void)
+{
+	int ch1a;
+	int ch1b;
+	int ch2a;
+	int ch2b;
+	int newline = 10;
+
+	ch1a = 48;
+	while (ch1a <= 57)
+	{
+		ch1b = 48;
+		while (ch1b <= 57)
+		{
+			ch2a = 48;
+			while (ch2a <= 57)
+			{
+				ch2b = 48;
+				while (ch2b <= 57)
+				{
+					if ((ch1a < ch2a) || ((ch1a == ch2a) && (ch1b < ch2b)))
+					{
+						putchar(ch1a);
+						putchar(ch1b);
+						putchar(32);
+						putchar(ch2a);
+						putchar(ch2b);
+						if (!(ch1a == 9 && ch1b == 8))
+						{
+							putchar(',');
+							putchar(32);
+						}
+						ch2b++;
+					}
+					else
+					{
+						ch2b++;
+						continue;
+					}
+				}
+				ch2a++;
+			}
+			ch1b++;
+		}
+		ch1a++;
+	}
+	putchar(newline);
+	return (0);
+}
