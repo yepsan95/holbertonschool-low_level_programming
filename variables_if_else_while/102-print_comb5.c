ffebf1832e64478759ba38aec539c6cc089999ed
Bryan Yep <yepsan95@gmail.com>
2022-10-05 08:54:17 -0700

    Prints all two digit number combinations

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -10,19 +10,14 @@ int main(void)
 	int ch1b;
 	int ch2a;
 	int ch2b;
-	int newline = 10;
 
-	ch1a = 48;
-	while (ch1a <= 57)
+	for (ch1a = 48; ch1a <= 57; ch1a++)
 	{
-		ch1b = 48;
-		while (ch1b <= 57)
+		for (ch1b =  48; ch1b <= 57; ch1b++)
 		{
-			ch2a = 48;
-			while (ch2a <= 57)
+			for (ch2a = 48; ch2a <= 57; ch2a++)
 			{
-				ch2b = 48;
-				while (ch2b <= 57)
+				for (ch2b = 48; ch2b <= 57; ch2b++)
 				{
 					if ((ch1a < ch2a) || ((ch1a == ch2a) && (ch1b < ch2b)))
 					{
@@ -31,25 +26,22 @@ int main(void)
 						putchar(32);
 						putchar(ch2a);
 						putchar(ch2b);
-						if (!(ch1a == 57 && ch1b == 56 && ch2a == 57 && ch2b == 57))
-						{
-							putchar(',');
-							putchar(32);
-						}
-						ch2b++;
 					}
 					else
 					{
-						ch2b++;
 						continue;
 					}
+					if (!(ch1a == 57 && ch1b == 56 && ch2a == 57 && ch2b == 57))
+					{
+						putchar(',');
+						putchar(32);
+					}
+					else
+						continue;
 				}
-				ch2a++;
 			}
-			ch1b++;
 		}
-		ch1a++;
 	}
-	putchar(newline);
+	putchar(10);
 	return (0);
 }
