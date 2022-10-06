52c7a9ce5e5c5f7427873bf65e30b9b7b95a5cf1
Bryan Yep <yepsan95@gmail.com>
2022-10-06 11:28:55 -0700

    Evaluates if a number is positive, negative or zero

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Evaluates if a number is positive or negative
+ * @n: number to be evaluated
+ *
+ * Return: int
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar('-')
+	}
+}
