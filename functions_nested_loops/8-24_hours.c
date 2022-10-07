4c4c10226d336429cd1c54147a75aa4c5c596cd2
Bryan Yep <yepsan95@gmail.com>
2022-10-06 20:26:40 -0700

    Prints all 24 hours in a day

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int h1;
+	int h2;
+	int m1;
+	int m2;
+
+	for (h1 = '0'; h1 <= '2'; h1++)
+	{
+		for (h2 = '0'; h2 <= '9'; h2++)
+		{
+			for (m1 = '0'; m1 <= '5'; m1++)
+			{
+				for (m2 = '0'; m2 <= '9'; m2++)
+				{
+					_putchar(h2);
+					_putchar(m1);
+					_putchar(m2);
+				}
+			}
+		}
+	}
+}
