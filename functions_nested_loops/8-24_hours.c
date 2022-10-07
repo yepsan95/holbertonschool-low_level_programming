4c45811dfd6d2ab11f02b60f4b032e2d0410877b
Bryan Yep <yepsan95@gmail.com>
2022-10-06 20:37:01 -0700

    Prints all 24 hours in a day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -16,17 +16,22 @@ void jack_bauer(void)
 	{
 		for (h2 = '0'; h2 <= '9'; h2++)
 		{
-			for (m1 = '0'; m1 <= '5'; m1++)
-			{
-				for (m2 = '0'; m2 <= '9'; m2++)
+			if (h1 == '2' && h2 == '3' && m1 == '6')
+				break;
+			else
+			{	
+				for (m1 = '0'; m1 <= '5'; m1++)
 				{
-					_putchar(h1);
-					_putchar(h2);
-					_putchar(':');
-					_putchar(m1);
-					_putchar(m2);
-					_putchar(10);
-				}
+					for (m2 = '0'; m2 <= '9'; m2++)
+					{
+						_putchar(h1);
+						_putchar(h2);
+						_putchar(':');
+						_putchar(m1);
+						_putchar(m2);
+						_putchar(10);
+					}
+					}
 			}
 		}
 	}
