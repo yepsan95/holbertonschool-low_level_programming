d291dad36534b2f8a3fc20ae1c14033788ae81b1
Bryan Yep <yepsan95@gmail.com>
2022-10-06 20:09:12 -0700

    Prints the last digit of a number

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,15 @@
 int print_last_digit(int n)
 {
 	int d = n % 10;
-	_putchar(d + 48);
-	return (d);
+
+	if (d < 0)
+	{
+		_putchar(d - d * 2);
+		return (d - d * 2);
+	}
+	else
+	{
+		_putchar(d + 48);
+		return (d);
+	}
 }
