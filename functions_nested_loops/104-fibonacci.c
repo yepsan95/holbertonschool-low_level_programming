bdea3187c7ee674f6cd8e6c017494b35c7b58d0b
Bryan Yep <yepsan95@gmail.com>
2022-10-08 11:26:36 -0700

    Prints the first 98 Fibonacci numbers

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -11,26 +11,28 @@ int main(void)
 	unsigned long int x = 0;
 	unsigned long int y = 0;
 	unsigned long int z = 1;
-	unsigned long int d = 0;
-	unsigned long int rev = 0;
-
+	long double a;
+	long double b;
+	long double c;
+	
 	for (i = 0; i < 98; i++)
 	{
-		x = y;
-		y = z;
-		z = x + y;
-		if (z < 4294967295)
+		if (z < 429496729)
+		{
+			x = y;
+			y = z;
+			z = x + y;
+			a = (double)x / 10000000000000;
+			b = (double)y / 10000000000000;
+			c = (double)z / 10000000000000;
 			printf("%ld", z);
+		}
 		else
 		{
-			for (d = z; d != 0; d /= 10)
-				rev = rev * 10 + (d % 10);
-			d = rev;
-			while (rev != 0)
-			{
-				putchar((rev % 10) + 48);
-				rev /= 10;
-			}
+			a = b;
+			b = c;
+			c = a + b;
+			printf("%Lf", c);
 		}
 		if (i != 97)
 		{
