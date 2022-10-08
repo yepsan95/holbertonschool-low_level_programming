f1f164a4d341e2e48c113954da23287acb79f6ef
Bryan Yep <yepsan95@gmail.com>
2022-10-08 16:50:42 -0700

    Checks for uppercase character

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @c: character
+ * Return: 0 or 1
+ */
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
