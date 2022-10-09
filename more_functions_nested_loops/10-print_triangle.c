3f66f1eb1bccec178191695267d864d1dacac823
Bryan Yep <yepsan95@gmail.com>
2022-10-08 18:46:42 -0700

    Prints triangle of n size

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle of size n
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+	int z;
+
+	if (size <= 0)
+		_putchar(10);
+	for (x = size; x > 0; x--)
+	{
+		for (y = (size - 1); y < (size - (x + 1)); y++)
+			_putchar(32);
+		for (z = size; z > (size - 1); z--)
+			_putchar('#');
+		_putchar(10);
+	}
+}
