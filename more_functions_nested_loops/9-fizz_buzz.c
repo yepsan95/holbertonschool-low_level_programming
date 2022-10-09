cd688c7b56c14519c544559d96a6cc8be812d060
Bryan Yep <yepsan95@gmail.com>
2022-10-08 18:35:02 -0700

    Fizz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,14 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3) == 0 && (n % 5) != 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if ((n % 5) == 0 && (n % 3) != 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if ((n % 5) == 0 && (n % 3) == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else
 			printf("%d ", n);
+		putchar(32);
 	}
 	putchar(10);
 	return (0);
