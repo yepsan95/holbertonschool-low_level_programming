aa1bab2b0194419f44d508c6f50b527f94ed1664
Bryan Yep <yepsan95@gmail.com>
2022-10-08 17:14:38 -0700

    Prints the digits from 0 to 9 but 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the single digit numbers from 0 to 9 but 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		if (n != 50 && n != 52)
+			_putchar(n);
+	putchar(10);
+}
