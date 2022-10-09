571ac8070bb687c6d7224bdd2dc82e733a03aa65
Bryan Yep <yepsan95@gmail.com>
2022-10-08 17:05:52 -0700

    Prints the single digit numbers from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9
+ * 
+ * Return: coid
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 97; n++)
+		_putchar(n);
+	_putchar(10);
+}
