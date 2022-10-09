b7d44be672843b5aae0ed11abad00a3e5ff88ef0
Bryan Yep <yepsan95@gmail.com>
2022-10-08 17:35:56 -0700

    Prints line of n length

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,12 +3,12 @@
 /**
  * print_line - Prints a straight line of n length
  *
- * Return: int
+ * Return: voidi
  */
 void print_line(int n)
 {
-	for (n = n; n > 0 ; n--)
+	for (; n > 0 ; n--)
 		_putchar('_');
-	if (n == 0)
+	if (n => 0)
 		_putchar(10);
 }
