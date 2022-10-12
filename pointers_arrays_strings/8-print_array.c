03953ff3138157aba723719507397ed0ac8bb7df
Bryan Yep <yepsan95@gmail.com>
2022-10-12 12:09:58 -0700

    Prints n elements of anarray

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -18,12 +18,10 @@ void print_array(int *a, int n)
 		size++;
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
 		if (i < (n - 1))
-		{
-			_putchar(',');
-			_putchar(32);
-		}
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
 	}
 	_putchar(10);
 }
