d5c8e31c3d4560ae16453a1c8d064bf9077f6a5d
Bryan Yep <yepsan95@gmail.com>
2022-10-12 13:03:42 -0700

    Copies a string to a buffer

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -15,7 +15,7 @@ char *_strcpy(char *dest, char *src)
 	size = 0;
 	while (*(src + size) != '\0')
 		size++;
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 	{
 		*(dest + i) = src[i];
 	}
