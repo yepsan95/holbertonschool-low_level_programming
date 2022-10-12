d4ab0e950c1326fddbc3325c34e61427d55685f9
Bryan Yep <yepsan95@gmail.com>
2022-10-12 11:27:57 -0700

    Prints every other character of an string starting with the first

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string (odd)
+ * @str: string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int size;
+	int i;
+
+	for (size = 0; *str != '\0', size++)
+	for (i = 0; i <= size; i = i + 2)
+		_putchar(*(str[i]));
+}
+
