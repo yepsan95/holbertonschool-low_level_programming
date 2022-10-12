2fb43fd873f58a56370b9c2585ad4148e22555e1
Bryan Yep <yepsan95@gmail.com>
2022-10-12 12:00:18 -0700

    Prints n elements of anarray

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array name
+ * @n: number of elements
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int size;
+	int half;
+	int i;
+
+	size = 0;
+	while (*(str + size) != '\0')
+		size++;
+	for (i = 0; i < n; i++)
+		_putchar(a[i]);
+		_putchar(',');
+		_putchar(32);
+}
+
