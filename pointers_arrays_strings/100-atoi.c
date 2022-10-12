e40c6f68e51c08052a4fe5b37ddb7a6b87566eff
Bryan Yep <yepsan95@gmail.com>
2022-10-12 13:30:21 -0700

    Converts a string to an integer

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string
+ *
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int size;
+	int i;
+	int num;
+
+	size = 0;
+	while (*(src + size) != '\0')
+		size++;
+	i = size;
+	while (s[i] <= 48 && s[i] >= 57)
+	{
+		num = s[i] * pow(10, (size - i));
+		i--;
+	}
+}
+
