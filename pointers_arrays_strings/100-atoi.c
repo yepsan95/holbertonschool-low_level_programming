5def2c872f59cded58344174d211f68c690f9df7
Bryan Yep <yepsan95@gmail.com>
2022-10-13 08:43:04 -0700

    Converts a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,15 +9,12 @@
  */
 int _atoi(char *s)
 {
-	int size;
-	int i;
+	int size, i, primer_num, ult_num;
 	int num = 0;
 	int minus = 0;
-	int primer_num;
-	int ult_num;
 
-	for (size = 0; *(s + size) != '\0';)
-		size++;
+	for (size = 0; *(s + size) != '\0'; size++)
+
 	for (i = 0; i <= size; i++)
 	{
 		if (s[i] == '-')
