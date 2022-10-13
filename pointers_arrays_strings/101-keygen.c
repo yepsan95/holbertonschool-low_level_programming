00b0f4188a494ab6ef9c233037d723d8ec319215
Bryan Yep <yepsan95@gmail.com>
2022-10-13 11:47:03 -0700

    Creates random passwords

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include "main.h"
+
+/**
+ * main - C primary function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	int randomizer;
+	char numbers[] = "0123456789";
+	char letter[] = "abcdefghijklmnopqrstuvwxyz";
+	char LETTER[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char symbols[] = "!@#$^&*?";
+
+	randomizer = rand() % 4;
+	for(i = 0; i < N; i++)
+	{
+		if (randomizer == 2)
+		{
+			password[i] = numbers[rand() % 10];
+			randomizer = rand() % 4;
+		}
+	}
+	return (password);
+}
