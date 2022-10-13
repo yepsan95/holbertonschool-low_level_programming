cfff2da40306f5dd87ed4e1be78d7c6469e88ed5
Bryan Yep <yepsan95@gmail.com>
2022-10-13 10:39:21 -0700

    Appends a string to another string up to n bytes

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncat - appends an string to another string
+i * @dest: original string
+ * @src: string to be appended
+ * @n: number of bytes of src to be appended
+ *
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int x = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+	while (*(src + x) != '\0' && x <= n)
+	{
+		dest[i + x] = src[x];
+		x++;
+	}
+	return (dest);
+}
