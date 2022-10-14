3ac9f3e1dbfa371dbc935099bda302acddebf570
Bryan Yep <yepsan95@gmail.com>
2022-10-13 17:02:57 -0700

    Copies a string to another string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string into another
+i* @dest: destiny string
+ * @src: source string
+ * @n: number of bytes of src to be copied
+ *
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int x = 0;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+	return (*dest);
+}
