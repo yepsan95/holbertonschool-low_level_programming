b36ccb1101cc0b3814969dcdaf90c4100827b688
Bryan Yep <yepsan95@gmail.com>
2022-10-14 20:36:45 -0700

    Copies a string to another string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -19,6 +19,6 @@ char *_strncpy(char *dest, char *src, int n)
 		*(dest + i) = *(src + i);
 	if (size < (n + 1))
 		for (; i < n; i++)
-			*(dest + i) = '\0'
+			*(dest + i) = '\0';
 	return (dest);
 }
