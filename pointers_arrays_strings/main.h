ef6fb489a735b30539661d439f6d74ca8a5f9b32
Bryan Yep <yepsan95@gmail.com>
2022-10-14 21:07:14 -0700

    Main .h file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,4 +14,4 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
-nt _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2);
