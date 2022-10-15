0041e61c4da87326fb27fc69ff38142ad0bfd40f
Bryan Yep <yepsan95@gmail.com>
2022-10-15 14:56:32 -0700

    Locates a character in a string

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,7 +12,6 @@ char *_strchr(char *s, char c)
 {
 	int i;
 	int size = 0;
-	int p;
 
 	for (i = 0; s[i] != '\0'; i++)
 		size++;
@@ -20,11 +19,12 @@ char *_strchr(char *s, char c)
 	{
 		if (s[i] == c)
 		{
-			p = (s + i);
 			break;
+			return (s + i);
 		}
 		else
-			p = NULL;
+		{
+			return (NULL);
+		}
 	}
-	return (p);
 }
