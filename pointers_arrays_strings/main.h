9c632cb3dc1141c41cced4b2de1f51096f02466c
Bryan Yep <yepsan95@gmail.com>
2022-10-15 14:29:49 -0700

    Main .h file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,3 +19,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
