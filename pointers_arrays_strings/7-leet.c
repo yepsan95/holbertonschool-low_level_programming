b4e47f3d996c89dfca72e8944a6d617c21a98445
Bryan Yep <yepsan95@gmail.com>
2022-10-15 14:09:57 -0700

    Encodes a string into 1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,19 +9,18 @@ i* @a: string
 char *leet(char *a)
 {
 	int i ;
+	int j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "43071"
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		if (a[i] == 'a' || a[i] == 'A')
-			a[i] = '4';
-		else if (a[i] == 'e' || a[i] == 'E')
-			a[i] = '3';
-		else if (a[i] == 'o' || a[i] == 'O')
-			a[i] = '0';
-		else if (a[i] == 't' || a[i] == 'T')
-			a[i] = '7';
-		else if (a[i] == 'l' || a[i] == 'L')
-			a[i] = '1';
+		for (j = 0; j < 5; j++)
+		{
+			if (a[i] = letters[j])
+				a[i] = numbers[j / 2];
+		}
+
 	}
 	return (a);
 }
