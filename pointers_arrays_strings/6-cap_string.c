5abe9c850ea894187a44402f057462a24e5e33ef
Bryan Yep <yepsan95@gmail.com>
2022-10-15 13:52:26 -0700

    capitalizes all words of a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,6 +14,8 @@ char *cap_string(char *a)
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
+		if (a[0] >= 97 && a[0] >= 122)
+			a[0] = a[0] - 32;
 		for (j = 0; j <= 12; j++)
 		{
 			if (a[i] == sep[j] && a[i + 1] >= 97 && a[i + 1] <= 122)
