8191684d393aa79a0aaacb8744a84538a2363f9e
Bryan Yep <yepsan95@gmail.com>
2022-10-16 09:32:19 -0700

    Locates a substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - locates a substring
+i* @haystack: pointer to main string
+ * @needle: pointer to substring
+ *
+ * Return: char *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	int subsize = 0;
+	int coincidences = 0;
+
+	for (i = 0; needle[i] != '\0'; i++)
+		subsize++;
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] == needle[j])
+				coincidences++;
+			else
+				break;
+		}
+		if (coincidences == (subsize - 1))
+			return (haystack + i);
+	}
+	if (coincidences < (subsize - 1))
+		return (NULL);
+}
