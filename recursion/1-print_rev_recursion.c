fb989e82b8a2a27aad4c266eadc280f1f699055f
Bryan Yep <yepsan95@gmail.com>
2022-10-20 08:59:22 -0700

    Prints a string in reverse with recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a reversed string with recursion
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+}
