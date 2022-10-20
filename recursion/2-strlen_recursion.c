0b53657fb53ad1d31d6724f3b637cebc344d9907
Bryan Yep <yepsan95@gmail.com>
2022-10-20 10:22:59 -0700

    Returns the length of a string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	int contador;
+	if (*s == '\0')
+		return (0);
+	contador = _strlen_recursion(s + 1);
+	return (contador + 1);
+}
