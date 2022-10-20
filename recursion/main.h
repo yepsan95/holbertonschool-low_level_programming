39e769fdc5134f96ffba13f59c88192e01491168
Bryan Yep <yepsan95@gmail.com>
2022-10-20 11:28:21 -0700

    Main .h file

--- recursion/main.h
+++ recursion/main.h
@@ -4,3 +4,5 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _sqrt_recursion(int n);
 int sqrt_fb(int n, int contador);
+int is_prime_number(int n);
+int is_prime(int contador, int n);
