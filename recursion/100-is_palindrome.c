30f9c239cd6c4b4d678974b755395a09f35c8bcd
Bryan Yep <yepsan95@gmail.com>
2022-10-20 23:06:51 -0700

    Checks if a string is a palindrome

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * find_strlen - Returns the length of a string
+ * @s: string
+ *
+ * Return: length of the string
+ **/
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{				
+		len++;
+		len += find_strlen(s + len);
+	}
+	return (len);
+}
+
+/**
+ * check_palindrome - Checks if a string is a palindrome
+ * @s: string
+ * @len: length of the string
+ * @i: The index of the string
+ *
+ * Return: 1 or 0
+ **/
+int check_palindrome(char *s, int len, int i)
+{
+	if (s[i] == s[len / 2])
+		return (1);
+	if (s[i] == s[len - i - 1])
+		return (check_palindrome(s, len, i + 1));
+	return (0);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: string
+ *
+ * Return: 1 or 0
+ **/
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
+		return (1);
+	return (check_palindrome(s, len, i));
+}
