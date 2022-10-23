65f9e7abb2cea45dafd2f5bda6b7e283822d22c9
Bryan Yep <yepsan95@gmail.com>
2022-10-23 16:39:24 -0700

    Prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints the name of the program
+*
+* @argc: argument count
+* @argv: argument vector
+*/
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
