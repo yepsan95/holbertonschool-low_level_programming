47984f3c559b68483b81a8dcb8a17feefe4b1819
Bryan Yep <yepsan95@gmail.com>
2022-10-23 16:50:55 -0700

    Prints its name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
 * @argc: argument count
 * @argv: argument vector
 */
-int main(int argc_attribute_((unused)), char *argv[])
+int main(_attribute_((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
