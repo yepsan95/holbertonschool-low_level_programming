ea680963082dabad72fa98c2167ff7211408125f
Bryan Yep <yepsan95@gmail.com>
2022-10-23 19:38:24 -0700

    Add the numbers passed as arguments to the program, adds them and prints the result

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -11,6 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int i;
+	int j;
 	int sum = 0;
 
 	if (argc == 1)
@@ -18,8 +19,18 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (1);
 	}
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 		sum = sum + atoi(argv[i]);
+	}
 	printf("%d\n", sum);
 	return (0);
 }
