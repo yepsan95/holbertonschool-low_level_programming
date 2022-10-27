b3b79a2ee6fbbc72cc11eb9718b23f058906c447
Bryan Yep <yepsan95@gmail.com>
2022-10-27 12:59:34 -0700

    Creates an array of chars

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -16,8 +16,10 @@ char *create_array(unsigned int size, char c)
 
 	puntero = malloc(size * sizeof(char));
 	if (puntero == NULL || size == 0)
+	{
 		free(puntero);
 		return (NULL);
+	}
 	*puntero = c;
 	free(puntero);
 	return (puntero);
