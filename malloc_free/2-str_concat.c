92e8bcfe13d70067fcfc4a61b71afb1921aa7520
Bryan Yep <yepsan95@gmail.com>
2022-10-29 16:33:37 -0700

    Concatenates two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -24,9 +24,9 @@ char *str_concat(char *s1, char *s2)
 	puntero = (char *)malloc((size_s1 + size_s2) + sizeof(char));
 	if (puntero == NULL)
 		return (NULL);
-	for (i = 0; i <= size_s1; i++)
+	for (i = 0; i < size_s1; i++)
 		*(puntero + i) = s1[i];
 	for (i = 0; i <= size_s2; i++)
-		*(puntero + size_s1 + i) = s2[i];
+		*(puntero + size_s1 + 1 + i) = s2[i];
 	return (puntero);
 }
