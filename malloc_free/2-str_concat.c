880c40b1358ce774a685ec6421334103c4357045
Bryan Yep <yepsan95@gmail.com>
2022-10-29 16:26:00 -0700

    Concatenates two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -4,33 +4,29 @@
 
 /**
  * str_concat - concatenates two strings
- *
  * @s1: pointer to string 1
  * @s2: pointer to string 2
  *
- * Return: pointer to new string
+ * Return: pointer to concatenated string
  */
 char *str_concat(char *s1, char *s2)
 {
+	char *puntero;
+	int size_s1;
+	int size_s2;
 	int i;
-	int s1_size;
-	int s2_size;
-	char *rtn;
 
 	for (i = 0; s1[i] != '\0'; i++)
-	s1_size = i;
+	size_s1 = i - 1;
 	for (i = 0; s2[i] != '\0'; i++)
-	s2_size = i;
-	rtn = malloc(sizeof(char) * (s1_size + s2_size + 1));
-	if (rtn == NULL)
-	{
-		free(rtn);
+	size_s2 = i - 1;
+
+	puntero = (char *)malloc((size_s1 + size_s2) + sizeof(char));
+	if (puntero == NULL)
 		return (NULL);
-	}
-	for (i = 0; i < s1_size; i++)
-		*(rtn + i) = s1[i];
-	for (i = 0; i <= s2_size; i++)
-		*(rtn + s1_size + i) = s2[i];
-	free(rtn);
-	return (rtn);
+	for (i = 0; i <= size_s1; i++)
+		*(puntero + i) = s1[i];
+	for (i = 0; i <= size_s2; i++)
+		*(puntero + size_s1 + i) = s2[i];
+	return (puntero);
 }
