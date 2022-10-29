3496f56709f7484e4bdfc3d7cc23873e0b8f3df4
Bryan Yep <yepsan95@gmail.com>
2022-10-29 15:41:46 -0700

    Copies a string into a newly allocated space in memory

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copies a string into a newly allocated space o memory
+ *
+ * @str: pointer to string
+ *
+ * Return: pointer to newly allocated space in memory
+ */
+char *_strdup(char *str)
+{
+	char *puntero;
+	int i;
+	int str_size;
+
+	for (i = 0; str[i] = '\0'; i++)
+	str_size = i;
+	puntero = (char *)malloc(str_size * sizeof(char));
+	if (puntero == NULL || str = NULL)
+		return (NULL);
+	for (i = 0; i < str_size; i++)
+		*(puntero + i) = str[i];
+	return (puntero);
+}
