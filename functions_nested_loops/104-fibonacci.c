d2b2e260a1efbbd6c792afcd790de5a8d3cdf385
Bryan Yep <yepsan95@gmail.com>
2023-02-23 14:24:38 -0800

    Finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -8,32 +8,43 @@
 int main(void)
 {
 	int i;
-	unsigned long int x = 0;
-	unsigned long int y = 0;
-	unsigned long int z = 1;
+	unsigned long int a, b, sum;
+	unsigned long int a_1, a_2, b_1, b_2, sum_1, sum_2;
 
-	for (i = 0; i < 91; i++)
+	a = 0;
+	b = 1;
+	sum = a + b;
+
+	for (i = 1; i <= 91; i++)
 	{
-		if (z < 7540113804746346429)
+		printf("%ld\n", sum);
+		a = b;
+		b = sum;
+		sum = a + b;
+	}
+
+	a_1 = a / 10000000000;
+	a_2 = a % 10000000000;
+	b_1 = b / 10000000000;
+	b_2 = b % 10000000000;
+	sum_1 = a_1 + b_1;
+	sum_2 = a_2 + b_2;
+
+	for (i = 92; i <= 97; i++)
+	{
+		printf("%ld%ld\n", sum_1, sum_2);
+		a_1 = b_1;
+		a_2 = b_2;
+		b_1 = sum_1;
+		b_2 = sum_2;
+		sum_1 = a_1 + b_1;
+		sum_2 = a_2 + b_2;
+		if (sum_2 > 9999999999)
 		{
-			x = y;
-			y = z;
-			z = x + y;
-			printf("%ld", z);
-		}
-		if (i != 97)
-		{
-			putchar(',');
-			putchar(32);
+			sum_1 = sum_1 + 1;
+			sum_2 = sum_2 % 10000000000;
 		}
 	}
-	printf("%d%ld%s", 1220016041, 5121876738, ", ");
-	printf("%d%ld%s", 1974027421, 9868223167, ", ");
-	printf("%ld%ld%s", 3194043463, 4990099905, ", ");
-	printf("%ld%ld%s", 5168070885, 4858323072, ", ");
-	printf("%ld%ld%s", 8362114348, 9848422977, ", ");
-	printf("%d%ld%s", 1353018523, 44706746049, ", ");
-	printf("%ld%ld", 2189229958, 34555169026);
-	putchar(10);
+
 	return (0);
 }
