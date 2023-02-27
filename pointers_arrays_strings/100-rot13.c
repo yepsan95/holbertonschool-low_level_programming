2ab684de68e52c5e645f97f85dd8a0e5228a1751
Bryan Yep <yepsan95@gmail.com>
2023-02-27 11:21:39 -0800

    Encodes a string using rot13

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -8,16 +8,19 @@
  */
 char *rot13(char *a)
 {
-	char lower[52] = "AaBbCcDdEeFfGgHhIiJjKkLlMmzZyYxXwWvVuUtTsSrRqQpPoOnN";
+	char set1[27] = "AaBbCcDdEeFfGgHhIiJjKkLlMm";
+	char set2[27] = "NnOoPpQqRrSsTtUuVvWwXxYyZz";
 	int i;
 	int j;
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		for (j = 0; j != '\0'; j++)
+		for (j = 0; j <= 25; j++)
 		{
-			if (a[i] == lower[j] && a[i] != '\0')
-				a[i] = lower[51 - j];
+			if (a[i] == set1[j] && a[i] != '\0')
+				a[i] = set2[j];
+			else if (a[i] == set2[j] && a[i] != '\0')
+				a[i] = set1[j];
 		}
 	}
 	return (a);
