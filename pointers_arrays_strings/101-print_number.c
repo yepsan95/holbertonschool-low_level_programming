a37de51359342e9cbd5620d55d5b0bd7b070ae9a
Bryan Yep <yepsan95@gmail.com>
2023-02-27 19:49:36 -0800

    Prints an integer

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -17,25 +17,25 @@ void print_number(int n)
 	else if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
 	}
-	if (n > 0)
+
+	size = 0;
+	i = n;
+	while (i != 0)
 	{
-		size = 0;
-		i = n;
-		while (i > 0)
+		i = i / 10;
+		size++;
+	}
+	for (i = size; i > 0; i--)
+	{
+		digit = n;
+		for (j = i; j > 1; j--)
 		{
-			i = i / 10;
-			size++;
+			digit = digit / 10;
 		}
-		for (i = size; i > 0; i--)
-		{
-			digit = n;
-			for (j = i; j > 1; j--)
-			{
-				digit = digit / 10;
-			}
-			_putchar((digit % 10) + 48);
-		}
+		digit = digit % 10;
+		if (digit < 0)
+			digit = digit * -1;
+		_putchar(digit + 48);
 	}
 }
