f1e38cc623231cde9cb6b0ffc10f57fb5b20fe84
Bryan Yep <yepsan95@gmail.com>
2023-03-05 13:38:23 -0800

    Adds two numbers

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -7,12 +7,12 @@
  * @r: buffer
  * @size_r: buffer size
  *
- * Return: char *
+ * Return: the sum of n1 and n2; 0 if buffer is insufficient
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	char *longest_str;
-	int i, n1_len, n2_len, shortest_len, digit, add1;
+	int i, n1_len, n2_len, shortest_len, longest_len, digit, add1;
 
 	for (i = 0; n1[i] != '\0'; i++)
 		n1_len = i + 1;
@@ -21,16 +21,19 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if ((n1_len - n2_len) < 0)
 	{
 		shortest_len = n1_len;
+		longest_len = n2_len;
 		longest_str = n2;
 	}
 	else
 	{
 		shortest_len = n2_len;
+		longest_len = n1_len;
 		longest_str = n1;
 	}
+	if ((n1_len + 1) >= size_r || (n2_len + 1) >= size_r)
+		return (0);
 	add1 = 0;
-	r[size_r - 1] = '\0';
-	for (i = 1; i <= size_r - 1; i++)
+	for (i = 1; i <= longest_len; i++)
 	{
 		if (i <= shortest_len)
 			digit = ((n1[n1_len - i]) - 48) + (n2[(n2_len - i)] - 48) + add1;
@@ -42,9 +45,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			digit = digit % 10;
 			add1 = 1;
 		}
-		r[(size_r - 1) - i] = digit + 48;
+		r[i - 1] = digit + 48;
 	}
 	if (add1 == 1)
-		r[0] = add1 + 48;
+		r[i - 1] = add1 + 48;
+	rev_string(r);
 	return (r);
 }
