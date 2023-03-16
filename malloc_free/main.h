a7fd84bbcd1f0b9c192cc2c3dda56bb121e02232
Bryan Yep <yepsan95@gmail.com>
2023-03-16 09:37:36 -0700

    main.h

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -3,3 +3,8 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int char_count(char *str, int *j, int *m);
+int word_count(char *str);
+
