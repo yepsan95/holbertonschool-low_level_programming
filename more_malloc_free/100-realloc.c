c0523a969bc3bcfdf4e3b3afceecc1b6896d6808
Bryan Yep <yepsan95@gmail.com>
2023-03-16 19:52:24 -0700

    Reallocates a memory block

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -22,6 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	if (ptr == NULL)
 		return (new_ptr);
+	new_ptr = (typeof(ptr))new_ptr;
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
@@ -33,5 +34,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			new_ptr[i] = ptr[i];
 	}
 	free(ptr);
-	return ((void *)new_ptr);
+	return (new_ptr);
 }
