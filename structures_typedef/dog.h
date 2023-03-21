cd20b46663a9750a1eb2094ae7b74a6f3a79cbd5
Bryan Yep <yepsan95@gmail.com>
2023-03-20 19:06:12 -0700

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - stores the data of adog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: stores the name, age and owner of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
