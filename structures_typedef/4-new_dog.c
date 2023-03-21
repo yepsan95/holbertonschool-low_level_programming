7438259448b2a3178eb0a9db8a523bbed76f326e
Bryan Yep <yepsan95@gmail.com>
2023-03-21 16:48:58 -0700

    Creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *dog;
 	char *name_copy;
 	char *owner_copy;
 	int i, name_len, owner_len;
@@ -20,32 +20,34 @@ dog_t *new_dog(char *name, float age, char *owner)
 	name_len = 0;
 	owner_copy = 0;
 
-	new_dog = (dog_t *)malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	dog = (dog_t *)malloc(sizeof(dog_t));
+	if (dog == NULL)
 		return (NULL);
 	for (i = 0; name[i] != '\0'; i++)
 		name_len++;
 	name_copy = (char *)malloc((name_len + 1) * sizeof(char));
 	if (name_copy == NULL)
 	{
-		free(new_dog);
+		free(dog);
 		return (NULL);
 	}
-	for (i = 0; i < name_len; i++)
+	for (i = 0; name[i] != '\0'; i++)
 		name_copy[i] = name[i];
-	new_dog->name = name_copy;
-	new_dog->age = age;
+	name_copy[i] = '\0';
+	dog->name = name_copy;
+	dog->age = age;
 	for (i = 0; owner[i] != '\0'; i++)
 		owner_len++;
 	owner_copy = (char *)malloc(sizeof((owner_len + 1) * sizeof(char)));
 	if (owner_copy == NULL)
 	{
-		free(new_dog);
+		free(dog);
 		free(name_copy);
 		return (NULL);
 	}
-	for (i = 0; i < owner_len; i++)
+	for (i = 0; owner[i] != '\0'; i++)
 		owner_copy[i] = owner[i];
-	new_dog->owner = owner_copy;
-	return (new_dog);
+	owner_copy[i] = '\0';
+	dog->owner = owner_copy;
+	return (dog);
 }
