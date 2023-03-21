9ae7c2529d57a072bc6a24c8369e4e80ea5296ad
Bryan Yep <yepsan95@gmail.com>
2023-03-20 20:02:33 -0700

    Creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,7 +12,10 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
+	struct dog_t new_dog;
+
+	new_dog.name = name;
+	new_dog.age = age;
+	new_dog.owner = owner;
+	return (new_dog);
 }
