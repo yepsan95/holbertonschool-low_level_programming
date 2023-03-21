79739447d47fb31efa82b3b0e69715fd1532673a
Bryan Yep <yepsan95@gmail.com>
2023-03-20 19:57:03 -0700

    Creates a new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog_t variable
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: void
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+}
