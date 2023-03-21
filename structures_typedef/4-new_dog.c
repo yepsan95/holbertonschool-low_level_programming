839854637e0bc8e8406333f0dd654c95d956729e
Bryan Yep <yepsan95@gmail.com>
2023-03-20 20:31:23 -0700

    Creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,12 +15,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 	char *name_copy;
 	char *owner_copy;
-	int i;
+	int i, name_len, owner_len;
+
+	name_len = 0;
+	owner_copy = 0;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	name_copy = malloc(sizeof(name));
+	for (i = 0; name[i] != '\0'; i++)
+		name_len++;
+	name_copy = malloc(sizeof(name_len + 1));
 	if (name_copy == NULL)
 	{
 		free(new_dog);
@@ -31,7 +36,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	name_copy[i] = '\0';
 	new_dog->name = name_copy;
 	new_dog->age = age;
-	owner_copy = malloc(sizeof(owner));
+	for (i = 0; owner[i] != '\0'; i++)
+		owner_len++;
+	owner_copy = malloc(sizeof(owner_len + 1));
 	if (owner_copy == NULL)
 	{
 		free(new_dog);
