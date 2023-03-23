60bd210b55cce6642036b5fb11f656433e2dc6d7
Bryan Yep <yepsan95@gmail.com>
2023-03-22 21:08:30 -0700

    Program that performs simple operations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -18,17 +18,17 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2][0] == '/' || argv[2][0] == '%') &&(atoi(argv[3]) == 0))
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[3]) == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 	result = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
-	/**if (result == NULL)
+	if (strlen(argv[2]) > 1)
 	{
 		printf("Error\n");
 		exit(99);
-	}*/
+	}
 	printf("%d\n", result);
 	return (0);
 }
