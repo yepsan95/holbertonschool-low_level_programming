65e3c2c92c78fb8eab317fdd8ef39683f1899c06
Bryan Yep <yepsan95@gmail.com>
2023-03-22 18:21:13 -0700

    executes a function given as a parameter on each element of an array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if (action == NULL || array = NULL)
+	if (action == NULL || array == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
