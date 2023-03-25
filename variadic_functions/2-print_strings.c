ce733fdda8051206ae9636a519e84a81675b204b
Bryan Yep <yepsan95@gmail.com>
2023-03-24 21:33:48 -0700

    Prints strings, followed by a new line

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -9,7 +9,7 @@
  *
  * Return: void
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list p;
 	unsigned int i;
