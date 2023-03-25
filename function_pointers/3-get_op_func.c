a553d819deed25a38c8688c62e56884e50573e50
Bryan Yep <yepsan95@gmail.com>
2023-03-24 17:01:30 -0700

    Performs sipmle operations

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -22,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i < 6)
+	while ((ops[i].op) != NULL)
 	{
 		if (*(s) == *(ops[i].op))
 			return (ops[i].f);
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -14,19 +14,23 @@ int main(int argc, char *argv[])
 {
 	int (*func)(int, int);
 	int result;
+	char *s;
+
+	s = argv[2];
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	func = (*get_op_func(argv[2]));
-	if (strlen(argv[2]) > 1 || (*func) == NULL)
+	func = get_op_func(s);
+	if ((*(s) != '+' && *(s) != '-' && *(s) != '*' && *(s) != '/' && *(s) != '%')
+			|| (strlen(s) != 1))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result = func(atoi(argv[1]), atoi(argv[3]));
+	result = (*func)(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", result);
 	return (0);
 }
