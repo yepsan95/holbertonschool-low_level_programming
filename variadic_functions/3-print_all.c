986fae32a963f747e22f72838120c6c58b0a7ebd
Bryan Yep <yepsan95@gmail.com>
2023-03-24 22:39:04 -0700

    Prints everything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: a list of types of arguments passed to the function
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list p;
+	unsigned int j, k, n;
+	types type[] = {
+		{'c', char_f},
+		{'i', int_f},
+		{'f', float_f},
+		{'s', string_f},
+		{'\0', NULL}
+		};
+
+	va_start(p, n);
+
+	j = 0;
+	k = 0;
+	while (format[j] != '\0')
+	{
+		while (type[k].type != '\0')
+		{
+			if (format[j] == type[k].type)
+			{
+				(*type[k].func)(&p);
+				if (format[j + 1] != '\0')
+					printf(", ");
+			}
+			k++
+		}
+		j++;
+	}
+	putchar(10);
+	va_end(p);
+}
+
+/**
+ * char_f - prints a char parameter
+ * @p: pointer to a va_list variable
+ *
+ * Return: void
+ */
+void char_f(va_list *p)
+{
+	printf("%c", va_arg(*(p), char));
+}
+
+/**
+ * int_f - prints an int parameter
+ * @p: pointer to a va_list variable
+ *
+ * Return: void
+ */
+void int_f(va_list *p)
+{
+	printf("%d", va_arg(*(p), int));
+}
+
+/**
+ * float_f - prints a float parameter
+ * @p: pointer to a va_list variable
+ *
+ * Return: void
+ */
+void float_f(va_list *p)
+{
+	printf("%f", va_arg(*(p), float));
+}
+
+/**
+ * string_f - prints a string parameter
+ * @p: pointer to a va_list variable
+ *
+ * Return: void
+ */
+void string_f(va_list *p)
+{
+	printf("%s", va_arg(*(p), char *));
+}
