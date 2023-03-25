d374d974c77807e9676ad000a326eb910a0de7ae
Bryan Yep <yepsan95@gmail.com>
2023-03-24 22:38:46 -0700

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,20 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+typedef struct Types {
+
+	char type;
+	void (*func)(va_list *);
+} types;
+
+void char_f(va_list *p);
+void int_f(va_list *p);
+void float_f(va_list *p);
+void string_f(va_list *p);
 
 #endif
