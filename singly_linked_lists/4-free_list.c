0a18f9ad984c3ca18730298fb17eb7e2e8d53b96
Bryan Yep <yepsan95@gmail.com>
2023-04-11 08:50:12 -0700

    Frees a list_t list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
