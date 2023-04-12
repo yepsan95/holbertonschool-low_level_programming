18c94588d88e591f0f2c22f3a085fd5d07ff8fd0
Bryan Yep <yepsan95@gmail.com>
2023-04-11 17:50:51 -0700

    Adds a new node at the beggining of a dlistint_t list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,8 +17,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	while ((*head)->prev != NULL)
-		(*head) = (*head)->prev;
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
