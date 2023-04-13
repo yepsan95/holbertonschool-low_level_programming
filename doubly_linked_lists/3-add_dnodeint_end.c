97e46f3a5b990d3e0b6ecacb16c2c676dacafb15
Bryan Yep <yepsan95@gmail.com>
2023-04-12 19:09:17 -0700

    Added aditional pointer

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,6 +18,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
 	last = *head;
 	while (last->next != NULL)
 		last = last->next;
