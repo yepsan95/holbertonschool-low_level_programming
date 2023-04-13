366714ddb7c419bf4483446a71df91179835ed3d
Bryan Yep <yepsan95@gmail.com>
2023-04-13 14:52:54 -0700

    Returns the nth node of a dlistint_t list

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i;
 	dlistint_t *node;
 
-	temp = head;
+	node = head;
 	for (i = 0; i < index; i++)
 	{
 		if (node->next != NULL)
