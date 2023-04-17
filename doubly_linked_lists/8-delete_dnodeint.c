2873de2624688d22d3936fcf5a024f18e0436601
Bryan Yep <yepsan95@gmail.com>
2023-04-16 17:46:35 -0700

    Deletes the node at index index of a dlistint_t linked list

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -12,6 +12,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp;
 
+	if (head == NULL)
+		return (-1);
+
 	if (index == 0)
 	{
 		(*head)->next->prev = NULL;
