9888cbbe186770a59702a23ea7aa229faac3f646
Bryan Yep <yepsan95@gmail.com>
2023-04-16 17:25:40 -0700

    Inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
+	temp = *h;
 	while (idx != 1)
 	{
 		temp = temp->next;
