748033c457bfdb05f4024055c0379b2eb6ad833c
Bryan Yep <yepsan95@gmail.com>
2023-04-25 09:52:16 -0700

    Prints the binary representation of a number

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,9 +9,17 @@
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
+	{
+		_putchar(0 + 48);
 		return;
+	}
+	if (n == 1)
+	{
+		_putchar(1 + 48);
+		return;
+	}
 
-	if (n > 0)
+	if (n > 1)
 		print_binary(n >> 1);
 
 	_putchar((n & 1) + 48);
