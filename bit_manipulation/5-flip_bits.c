c7aa857390b7f386315f64f0ca0ca010aba8f031
Bryan Yep <yepsan95@gmail.com>
2023-04-25 14:00:23 -0700

    returns the number of bits you would need to flip to get from one number to another

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* clear_bit - returns the number of bits you would need to flip
+*             in order to get from one number to another
+* @n: original number
+* @m: resultant number
+*
+* Return: the number of bits flipped to get from n to m
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flip;
+	int count;
+
+	count = 0;
+	
+	for (flip = n ^ m; flip > 0 ; flip = flip << 1)
+	{
+		if (flip & 1 == 1)
+			count++;
+	}
+
+	return (count);
+}
