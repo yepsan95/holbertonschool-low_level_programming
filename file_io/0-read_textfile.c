d438e0b23dd9c9166ee51c8184fcec10f618d7be
Bryan Yep <yepsan95@gmail.com>
2023-05-02 13:12:53 -0700

    feat: added read_textfile() function that reads a text file and prints it to the POSIX standard output

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it
+ *                 to the POSIX standard output
+ * @filename: asbolute path of the file
+ * @letters: the number of letters it whould read and print
+ *
+ * Return: the actual number of letters it could read an print,
+ *         if the file cannot be opened or read, returns 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t n_bytes;
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	buffer = (char *)malloc((letters + 1) * sizeof(char));
+	if (buffer == NULL)
+		return (0);
+	read(fd, buffer, letters);
+	n_bytes = write(fd, buffer, letters);
+
+	return (n_bytes);
+}
