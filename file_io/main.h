69f38f17a5a50441c3473efb9cf96ea894f13821
Bryan Yep <yepsan95@gmail.com>
2023-05-03 16:15:56 -0700

    fix: updated header file

--- file_io/main.h
+++ file_io/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -11,5 +12,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int cp(const char *file_from, const char *file_to);
 
 #endif /*MAIN_H*/
