57df6bd59de89db3e47aadce3279b4b3b00d113c
Bryan Yep <yepsan95@gmail.com>
2023-05-04 14:09:51 -0700

    fix: fixed delimiter character problem

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -33,8 +33,8 @@ void print_all(const char * const format, ...)
 			{
 				printf("%s", delimitador);
 				(*type[k].func)(&p);
-				delimitador[0] = ' ';
-				delimitador[1] = ',';
+				delimitador[0] = ',';
+				delimitador[1] = ' ';
 				delimitador[2] = '\0';
 				break;
 			}
