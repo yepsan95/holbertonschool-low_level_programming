bd475ef7bb83fbcf1747e599ed320c7df300d635
Bryan Yep <yepsan95@gmail.com>
2023-05-04 11:35:31 -0700

    fix: updated header file

--- file_io/main.h
+++ file_io/main.h
@@ -13,5 +13,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int cp(const char *file_from, const char *file_to);
+void exit_97(char *filename);
+void exit_98(char *file_from);
+void exit_99(char *file_to);
+void exit_100(int fd);
 
 #endif /*MAIN_H*/
