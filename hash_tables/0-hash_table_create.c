528899463df8ebb70fe5ee570be83f7195153144
Bryan Yep <yepsan95@gmail.com>
2023-05-05 19:57:43 -0700

    feat: created function that creates a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *hash_table;
+	hash_table_t *hash_table = NULL;
 
 	hash_table->size = size;
 	hash_table->array = malloc(size * sizeof(hash_node_t *));
