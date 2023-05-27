b2ec669e778362329acf40f565832714976ecdbc
Bryan Yep <yepsan95@gmail.com>
2023-05-27 14:03:59 -0700

    feat: added function that searches for a value in a sorted array of integers using the Binary search algorithm

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,5 +6,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t first, size_t last);
+int binary_recursion(int *array, size_t first, size_t last, int value);
 
 #endif /*SEARCH_ALGOS_H*/
