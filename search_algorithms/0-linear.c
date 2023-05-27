aeacd34925240f53915f0e451b711a0734f8fdd2
Bryan Yep <yepsan95@gmail.com>
2023-05-27 12:41:26 -0700

    feat: added function that searches for a value in an array of integers using the Linear search algorithm

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in ana rray of integers
+ *                 using the Linear search algorithm
+ * @array: array of integers
+ * @size: size of the array
+ * @value: value to be searched for
+ *
+ * Return: the first index of the array where value is located
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
